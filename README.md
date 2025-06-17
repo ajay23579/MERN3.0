083ab18a82e6f9618647d9912871d80bb5138346
ajay23579 <ajaysubedihtd7@gmail.com>
2025-06-17 19:47:11 +0545

    Node create api added

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # MERN3.0
+Week 1 :
+Backend:
+npm inti -y
+
+Week 2 : project in Backend
+
+Week 3 : React 
+
+
+Week 4: Frontend with Tailwind
+
+Week 5: Into typeScript Websocket
+
+Week 6: Ecommerce Start Backend in Posgres SQL
+
+Week 7 : Continue With intregration
+
+week 8 : Ecommerce Frontend
+
+week 9 : Integrate 
+
+week 10 : Admin pannel and apis
+
+Week 11 : Order stock and api integration + stock.io Client
+
+Week 12 : Review controller and other features 
+
+week 13 : cloud and hosting 
+
+Week 14 : Docker and other
+
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
+app.use(express.json())
 const connectToDatabase = require('./dataBase/index')
+const Blog = require('./model/blogModel')
 
 connectToDatabase()
 
@@ -11,14 +16,35 @@ app.get('/',(req,res)=>{
 })
 
 
-app.get('/about',(req,res)=>{
-    res.json({
-        message : "This is about page"
-    })
-})
+app.post("/blog",async(req,res)=>{
+    console.log(req.body)
+    const {title,subtitle,description,image} = req.body
+    if(!title || !description || !image || !subtitle){
+        return res.status(400).json({
+            message : "please provide title, description, subtitle, image"
+        })
+    }
+
+    try{
+    await Blog.create({
+        title : title,
+        subtitle : subtitle,
+        description : description,
+        image : image
+
+    }),
+    res.status(200).json({
+        message : "Blog api hit sucessfully"
+    });
+} catch (error){
+    console.error(error);
+    res.status(500).json({
+        message: "Error creating blog",
+        error: error.message
+    });}
+});
 
 
-
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("starting in port 3000")
 })
