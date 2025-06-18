3ab9f1bc9e9640d857919565256e36db9c9aa980
ajay23579 <ajaysubedihtd7@gmail.com>
2025-06-18 22:06:28 +0545

    Added multer config for the image

--- app.js
+++ app.js
@@ -1,50 +1,49 @@
+require("dotenv").config();
 
-require('dotenv').config()
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-const express = require('express')
-const app = express()
-app.use(express.json())
-const connectToDatabase = require('./dataBase/index')
-const Blog = require('./model/blogModel')
+const connectToDatabase = require("./dataBase/index");
+const Blog = require("./model/blogModel");
+const {storage, multer} = require("./middleware/multerConfig");
 
-connectToDatabase()
+const upload = multer({storage: storage});
 
-app.get('/',(req,res)=>{
-    res.json({
-        message : 'this is home page'
-    })
-})
+connectToDatabase();
 
-
-app.post("/blog",async(req,res)=>{
-    console.log(req.body)
-    const {title,subtitle,description,image} = req.body
-    if(!title || !description || !image || !subtitle){
-        return res.status(400).json({
-            message : "please provide title, description, subtitle, image"
-        })
-    }
-
-    try{
-    await Blog.create({
-        title : title,
-        subtitle : subtitle,
-        description : description,
-        image : image
-
-    }),
-    res.status(200).json({
-        message : "Blog api hit sucessfully"
-    });
-} catch (error){
-    console.error(error);
-    res.status(500).json({
-        message: "Error creating blog",
-        error: error.message
-    });}
+app.get("/", (req, res) => {
+  res.json({
+    message: "this is home page",
+  });
 });
 
+app.post("/blog", upload.single("image"), async (req, res) => {
+  console.log(req.body);
+  const {title, subtitle, description, image} = req.body;
+  if (!title || !description || !subtitle) {
+    return res.status(400).json({
+      message: "please provide title, description, subtitle, image",
+    });
+  }
 
-app.listen(process.env.PORT,()=>{
-    console.log("starting in port 3000")
-})
+  try {
+    await Blog.create({
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      image: image,
+    }),
+      res.status(200).json({message: "The Blog Sucessfully created"});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error creating blog",
+      error: error.message,
+    });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("starting in port 3000");
+});
